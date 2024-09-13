531d441fa7c385900cabdc7a4d698c7d25b61b74
kdbbrown <billy.kano@afric.ca>
2024-09-13 16:20:26 +0100

    Chore:client component

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MasterService} from "../../services/master.service";
+import {APIResponseModel, IDesignation} from "../../model/interface/role";
 
 @Component({
   selector: 'app-designation',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
 
+  designations: IDesignation[] = [
+    { designationId: 1, designation: "Software Engineer" },
+    { designationId: 2, designation: "Product Manager" },
+    { designationId: 3, designation: "Data Scientist" },
+    { designationId: 4, designation: "UX Designer" },
+    { designationId: 5, designation: "Quality Assurance" },
+    { designationId: 6, designation: "System Administrator" },
+    { designationId: 7, designation: "Business Analyst" },
+    { designationId: 8, designation: "Marketing Specialist" },
+    { designationId: 9, designation: "Sales Executive" },
+    { designationId: 10, designation: "HR Manager" },
+  ];
+  designationList: IDesignation[] = new Array<IDesignation>();
+  isLoader: boolean = false;
+  masterService = inject(MasterService)
+
+  ngOnInit() {
+    // this.getAllDesignation()
+  }
+
+  getAllDesignation() {
+    this.masterService.getDesignation().subscribe({
+      next: (res: APIResponseModel) => {
+        this.designationList = res.data
+      },
+      error: (err: Error) => {console.error(err)},
+      complete: () => {console.info('Completed')}
+    })
+  }
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,31 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IRole} from "../../model/interface/role";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent implements OnInit{
-roleList:any[] =[]
+export class RolesComponent implements OnInit {
+  roleList: IRole[] = [
+    { roleId: 1, role: "Admin" },
+    { roleId: 2, role: "User" },
+    { roleId: 3, role: "Moderator" },
+    { roleId: 4, role: "Editor" },
+    { roleId: 5, role: "Viewer" },
+    { roleId: 6, role: "Contributor" },
+    { roleId: 7, role: "Manager" },
+    { roleId: 8, role: "Developer" },
+    { roleId: 9, role: "Designer" },
+    { roleId: 10, role: "Analyst" },
+  ]
   http = inject(HttpClient)
 
   ngOnInit() {
-export class RolesComponent implements OnInit{
-
-  http = inject(HttpClient)
-
   }
 
   getAllRoles() {
-  this.http.get("http://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any) => {})
-  }
-  ngOnInit() {
-
-  }
-
-  getAllRoles() {
-  this.http.get("http://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe(res:any => {})
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any) => {
+        this.roleList = res.data;
+      }
+    )
   }
 }
