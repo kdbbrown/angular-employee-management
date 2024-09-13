841e9bc0c7cff545e22339093528e61ab9c061dc
kdbbrown <billy.kano@afric.ca>
2024-09-13 16:21:40 +0100

    Created side components

--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {APIResponseModel} from "../model/interface/role";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MasterService {
-
+http = inject(HttpClient)
   constructor() { }
+
+  getDesignation():Observable<APIResponseModel>{
+  return this.http.get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation")
+  }
 }
